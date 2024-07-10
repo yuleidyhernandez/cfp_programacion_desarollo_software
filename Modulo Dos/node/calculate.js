ce6226900f26aa49d387f123bf63444900e57e37
Yuleidy Johanna Hernández Carbajal <yuleidyhernandez@CFPBARTOLOMEAMBROSIO.COM>
2024-07-10 16:04:58 -0600

    Se agregaron cambio

---
+++ Modulo Dos/node/calculate.js
@@ -0,0 +1,16 @@
+function suma(a,b){
+    return a + b
+}
+
+function resta(a,b){
+    return a - b
+}
+
+function division(a,b){
+    return a / b
+}
+
+function multiplicacion(a,b){
+    return a * b
+}
+ module.exports ={suma,resta,division,multiplicacion};
---
+++ Modulo Dos/node/index.js
@@ -0,0 +1,16 @@
+let {suma:funcionSumar,resta:funcionRestar,division:funcionDividir,multiplicacion:funcionMultiplicar} = require('./calculate.js');
+
+
+console.log(funcionSumar(10,5));
+
+console.log(funcionRestar(10,5));
+
+console.log(funcionDividir(10,5));
+
+console.log(funcionMultiplicar(10,5));
+
+
+// let nombre ='Yuleidy';
+// console.log(nombre);
+
+// console.log(globalThis);
