bac1d4ba78fe5f8cf8e1a81453588dbe1ad01657
Yuleidy Johanna Hernández Carbajal <yuleidyhernandez@CFPBARTOLOMEAMBROSIO.COM>
2024-08-21 16:01:34 -0600

    Se agrego una carpeta nueva

--- Modulo-tres/prueba tecnica3/cortes_longuitud.js
+++ Modulo-tres/prueba tecnica3/cortes_longuitud.js
@@ -1,9 +1,33 @@
-let metros = 4;
-let cuerda = function(a){
+// let metros = 4;
+// let contador = 1
+// while (metros-1 <= contador){
+//     let cortes = metros / contador
+//     contador++
+//     console.log(cortes);
+//   };
+// let cortes = metros/
+// let longitud = metros /cortes;
+// let maxicimo =Math.pow(longitud,cortes);
+// console.log(maxicimo);
 
 
+const duplicar = (num) => {
+    num = num + num;
+    console.log(num);
+    if (num > 100) {
+        return 'Salir';
+    }
+    return duplicar(num);
+};
 
-    
-}
+console.log(duplicar(4));
 
-console.log(cuerda(metros));
+
+const fibonacci = (n) => {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+console.log(fibonacci(6));
